b5c9973eea1671b4c87ff1635b1199459c404589
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-07 18:51:22 +0200

    initila commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 
-int main() {
+using namespace std;
 
-	return 0;
+template<typename T>
+T sum(T n1, T n2)
+{
+    return n1 + n2;
+}
+template<typename T>
+T sub(T n1, T n2)
+{
+    return n1 - n2;
+}
+template<typename T>
+T mul(T n1, T n2)
+{
+    return n1 * n2;
+}
+template<typename T>
+T div(T n1, T n2)
+{
+    return (n1 == 0 || n2 == 0) ? 0 : n1 / n2;
+}
+
+
+int main()
+{
+    double n1, n2;
+    cout << "Enter first  number: ";
+    cin >> n1 ;
+    cout << "Enter second  number: ";
+    cin >> n2;
+    double (*action[4])(double, double) = { sum, sub, mul, div };
+    cout << "Enter op (0(+),1(-),2(*),3(/)): ";
+    
+    int op;
+    cin >> op;
+    cout << action[op](n1, n2) << endl;
+
+
+    return 0;
 }
