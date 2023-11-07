6a66d4d4ab3de400de6096888e9f84c57e5dbcd1
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-07 19:01:50 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
+using namespace::std;
+const int arraySize = 5;  
+
+int generateRandomNumber(int min, int max) {
+    return min + (rand() % (max - min + 1));
+}
+
+template <typename T>
+void calculateSum(const T* arrayA, const T* arrayB, T* arrayC, int size) {
+    for (int i = 0; i < size; i++) {
+        arrayC[i] = arrayA[i] + arrayB[i];
+    }
+}
 
 int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
 
-	return 0;
+    int arrayA[arraySize];
+    int arrayB[arraySize];
+    int arrayC[arraySize];
+
+
+    for (int i = 0; i < arraySize; i++) {
+        arrayA[i] = generateRandomNumber(1, 100);
+        arrayB[i] = generateRandomNumber(1, 100);
+    }
+
+ 
+    calculateSum(arrayA, arrayB, arrayC, arraySize);
+
+    cout << "Array A: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << arrayA[i] << " ";
+    }
+    cout << std::endl;
+
+    cout << "Array B: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << arrayB[i] << " ";
+    }
+    cout << std::endl;
+
+    cout << "Array C (Sum of A and B): ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << arrayC[i] << " ";
+    }
+    cout << std::endl;
+
+    return 0;
 }
